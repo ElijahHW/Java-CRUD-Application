8374888392bb3497be8eb4b7d6c33190654dd23a
AleksanderNilsson <71332420+AleksanderNilsson@users.noreply.github.com>
2021-05-18 14:51:32 +0200

    metode for å hente data fra table

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JFrame;
 
 public class Main {
 	JFrame frame = new JFrame("Application");
 
 	public static void main(String[] args) {
-		
+		System.out.println("test");
+		List<List<String>> loginList = dbConnection.getTable("employees");
+		for(List<String> item : loginList) {
+			System.out.println(item);
+		}
 		
 	}
 
