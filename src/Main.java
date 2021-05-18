2d5fcec37e5c20bac69cade732aa385c6aa88ca2
AleksanderNilsson <71332420+AleksanderNilsson@users.noreply.github.com>
2021-05-18 13:39:37 +0200

    Ny JFrame

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 import java.sql.SQLException;
+import javax.swing.JFrame;
 
 public class Main {
+	JFrame frame = new JFrame("Application");
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		dbConnection conn = new dbConnection();
-		try {
-			conn.open();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		
 		
 	}
 
