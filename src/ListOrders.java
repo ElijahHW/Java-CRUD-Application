b9abe93dfc3a6a37c908b79ab37cf1039d361ba5
Gaffail <kristoffer.roren@gmail.com>
2021-05-18 23:39:56 +0200

    Updated LiestOrders

--- src/ListOrders.java
+++ src/ListOrders.java
@@ -5,6 +5,8 @@ import javax.swing.table.TableRowSorter;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,10 +29,33 @@ public class ListOrders {
 	public ListOrders() {
 		
 		panel = new JPanel();
+		panel.setLayout(new GridBagLayout());
 		
-		panel.add(FilterSearchPanel(), BorderLayout.NORTH);
-		panel.add(SorterChoicePanel(), BorderLayout.EAST);
-		panel.add(ScrollPanel(), BorderLayout.CENTER); 
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 0.2;
+		c.weighty = 0.2;
+		c.fill = GridBagConstraints.BOTH;
+		panel.add(FilterSearchPanel(), c);
+		
+		c.gridx = 90;
+		c.gridy = 0;
+		c.gridheight = 100;
+		c.weightx = 0;
+		c.weighty = 0;
+		c.fill = GridBagConstraints.BOTH;
+		panel.add(SorterChoicePanel(), c);
+		
+		c.gridx = 0;
+		c.gridy = 10;
+		c.gridheight = 90;
+		c.gridwidth = 90;
+		c.weightx = 1;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		panel.add(ScrollPanel(), c); 
 	}
 	
 	
@@ -153,24 +178,17 @@ public class ListOrders {
 		
 		JLabel FilterButtonLabel = new JLabel("Filter by: ");
 		
-		JRadioButton OrderNumberButton = new JRadioButton("Order Number", true);
-		JRadioButton OrderDateButton = new JRadioButton("Order Date");
-		JRadioButton RequiredDateButton = new JRadioButton("Required Date");
-		JRadioButton ShippedDateButton = new JRadioButton("Shipped Date");
-		JRadioButton StatusButton = new JRadioButton("Status");
-		JRadioButton CommentsButton = new JRadioButton("Comments");
-		JRadioButton CustomerNumberButton = new JRadioButton("Customer Number");
+		String[] SearchArray = {"Order Number", 
+				"Order Date", 
+				"Required Date", 
+				"Shipped Date", 
+				"Status", 
+				"Comments", 
+				"Customer Number"};
 		
-		ButtonGroup SearchButtons = new ButtonGroup();
-		
-		//adds all the radio buttons to a buttonset so that only one will be active at a time
-		SearchButtons.add(OrderNumberButton);
-		SearchButtons.add(OrderDateButton);
-		SearchButtons.add(RequiredDateButton);
-		SearchButtons.add(ShippedDateButton);
-		SearchButtons.add(StatusButton);
-		SearchButtons.add(CommentsButton);
-		SearchButtons.add(CustomerNumberButton);
+		JComboBox<String> SearchFilterColumns = new JComboBox<String>(SearchArray);
+		SearchFilterColumns.setPreferredSize(new Dimension(100, 50));
+		SearchFilterColumns.setMaximumSize(new Dimension(200, 50));
 		
 		JButton ExportButton = new JButton("Export to File");
 		
@@ -179,14 +197,9 @@ public class ListOrders {
 		
 		//adds all the buttons to a panel as well to display them
 		panel.add(FilterButtonLabel);
-		panel.add(OrderNumberButton);
-		panel.add(OrderDateButton);
-		panel.add(RequiredDateButton);
-		panel.add(ShippedDateButton);
-		panel.add(StatusButton);
-		panel.add(CommentsButton);
-		panel.add(CustomerNumberButton);
+		panel.add(SearchFilterColumns);
 		panel.add(ExportButton);
+		
 		
 		//Creates a filechooser to select a folder
 		JFileChooser PathChooser = new JFileChooser();
@@ -213,7 +226,7 @@ public class ListOrders {
                 JTextField textField = (JTextField) e.getSource();
                 SearchString = textField.getText();
                 
-                //Checks what radio button is selected
+                //Checks what is selected
                 if (SearchString.length() == 0) {
                 	
                 	sorter.setRowFilter(null);
@@ -221,30 +234,7 @@ public class ListOrders {
                 	
                 	try {
                 		
-                		int SelectedSortButton = 0;
-                		
-                		if (OrderNumberButton.isSelected()) {
-                			
-                			SelectedSortButton = 0;
-                		} else if (OrderDateButton.isSelected()) {
-                			
-                			SelectedSortButton = 1;
-                		} else if (RequiredDateButton.isSelected()) {
-                			
-                			SelectedSortButton = 2;
-                		} else if (ShippedDateButton.isSelected()) {
-                			
-                			SelectedSortButton = 3;
-                		} else if (StatusButton.isSelected()) {
-                			
-                			SelectedSortButton = 4;
-                		} else if (CommentsButton.isSelected()) {
-                			
-                			SelectedSortButton = 5;
-                		} else {
-                			
-                			SelectedSortButton = 6;
-                		}
+                		int SelectedSortButton = SearchFilterColumns.getSelectedIndex();
                 		
                 		sorter.setRowFilter(RowFilter.regexFilter("(?i)" + SearchString, SelectedSortButton));
                 	} catch (PatternSyntaxException pse) {
