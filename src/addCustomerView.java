8056c6712e6b0347328efa9b7a12237199e38363
Gaffail <kristoffer.roren@gmail.com>
2021-05-19 20:36:44 +0200

    Made addCustomer look and feel better

--- src/addCustomerView.java
+++ src/addCustomerView.java
@@ -20,10 +20,16 @@ public class addCustomerView implements ActionListener {
 	private JButton addToDbButton;
 	private JButton resetButton;
 	private GridBagConstraints gbc = new GridBagConstraints();
+	private GridBagConstraints LeftCon, RightCon;
+	private Dimension PreferedDimension= new Dimension(100, 20);
 	
 	private boolean UniqueName = false;
+	private double CreditNumber = 0f;
 	
 	public addCustomerView() {
+		
+		UpdateConstraints();
+		
 		panel = new JPanel();
 		panel.setLayout(new GridBagLayout());
 		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
@@ -31,7 +37,7 @@ public class addCustomerView implements ActionListener {
 		responseText = new JLabel("");
 		responseText.setBounds(25, 450, 200, 25);
 		
-		gbc.fill = GridBagConstraints.BOTH; // these constraints will be added to a JPanel later
+		gbc.fill = GridBagConstraints.BOTH; 
 		gbc.weightx = 55;
 		gbc.weighty = 55;
 		panel.add((panelCustomerNumber()), gbc);
@@ -86,6 +92,25 @@ public class addCustomerView implements ActionListener {
 	}
 
 	
+	protected void UpdateConstraints() {
+		
+		LeftCon = new GridBagConstraints();
+		
+		LeftCon.fill = GridBagConstraints.HORIZONTAL;
+		LeftCon.weightx = 0.10;
+		LeftCon.gridx = 0;
+		LeftCon.gridy = 0;
+		LeftCon.gridwidth = 3;
+		
+		RightCon = new GridBagConstraints();
+		
+		RightCon.fill = GridBagConstraints.HORIZONTAL;
+		RightCon.weightx = 0.90;
+		RightCon.gridx = 3;
+		RightCon.gridy = 0;
+		RightCon.gridwidth = 6;
+	}
+	
 	//Action listeners
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -109,7 +134,7 @@ public class addCustomerView implements ActionListener {
 							postalCodeInput.getText(), 
 							countryInput.getText(),
 							salesRepNumberList.getSelectedItem().toString(),
-							creditLimitInput.getText());
+							CreditNumber + "");
 					
 					responseText.setText(respons);
 							
@@ -159,6 +184,7 @@ public class addCustomerView implements ActionListener {
 		
 		JPanel pane = new JPanel();
 		JLabel label = new JLabel("Customer number");
+		label.setPreferredSize(PreferedDimension);
 		customerNumberInput = new JTextField(20);
 		customerNumberInput.addKeyListener(new KeyAdapter() {
 			
@@ -182,7 +208,6 @@ public class addCustomerView implements ActionListener {
                 		textField.setText(SearchString.replaceAll("[^\\d]", ""));
                 	}
                 	
-                	boolean numberExists = false;
                 	List<List<String>> list = dbConnection.getTable("customers");
                 	for (List<String> row : list) {
                 		
@@ -199,12 +224,18 @@ public class addCustomerView implements ActionListener {
                 	if (!UniqueName) {
                 		
                 		responseText.setText("Customer Number already exists");
+                	} else {
+                		
+                		responseText.setText(null);
                 	}
                 }
 			}
 		});
-		pane.add(label);
-		pane.add(customerNumberInput);
+		
+		pane.setLayout(new GridBagLayout());
+
+		pane.add(label, LeftCon);
+		pane.add(customerNumberInput, RightCon);
 		
 		return pane;
 	}
@@ -213,9 +244,13 @@ public class addCustomerView implements ActionListener {
 		
 		JPanel pane = new JPanel();
 		JLabel label = new JLabel("Customer name");
+		label.setPreferredSize(PreferedDimension);
 		customerNameInput = new JTextField(20);
-		pane.add(label);
-		pane.add(customerNameInput);
+		
+		pane.setLayout(new GridBagLayout());
+		
+		pane.add(label, LeftCon);
+		pane.add(customerNameInput, RightCon);
 		
 		return pane;
 	}
@@ -224,9 +259,13 @@ public class addCustomerView implements ActionListener {
 		
 		JPanel pane = new JPanel();
 		JLabel label = new JLabel("Contacts first name");
+		label.setPreferredSize(PreferedDimension);
 		contactFirstNameInput = new JTextField(20);
-		pane.add(label);
-		pane.add(contactFirstNameInput);
+		
+		pane.setLayout(new GridBagLayout());
+		
+		pane.add(label, LeftCon);
+		pane.add(contactFirstNameInput, RightCon);
 		
 		return pane;
 	}
@@ -235,9 +274,13 @@ public class addCustomerView implements ActionListener {
 		
 		JPanel pane = new JPanel();
 		JLabel label = new JLabel("Contacts last name");
+		label.setPreferredSize(PreferedDimension);
 		contactLastNameInput = new JTextField(20);
-		pane.add(label);
-		pane.add(contactLastNameInput);
+		
+		pane.setLayout(new GridBagLayout());
+		
+		pane.add(label, LeftCon);
+		pane.add(contactLastNameInput, RightCon);
 		
 		return pane;
 	}
@@ -246,9 +289,13 @@ public class addCustomerView implements ActionListener {
 		
 		JPanel pane = new JPanel();
 		JLabel label = new JLabel("Phone");
+		label.setPreferredSize(PreferedDimension);
 		phoneInput = new JTextField(20);
-		pane.add(label); 
-		pane.add(phoneInput); 
+		
+		pane.setLayout(new GridBagLayout());
+		
+		pane.add(label, LeftCon); 
+		pane.add(phoneInput, RightCon); 
 		
 		return pane;
 	}
@@ -256,10 +303,14 @@ public class addCustomerView implements ActionListener {
 	private JPanel panelAdressOne() {
 		
 		JPanel pane = new JPanel();
-		JLabel label = new JLabel("Address line 1");		
+		JLabel label = new JLabel("Address line 1");	
+		label.setPreferredSize(PreferedDimension);
 		addressLine1Input = new JTextField(20);
-		pane.add(label);
-		pane.add(addressLine1Input);
+		
+		pane.setLayout(new GridBagLayout());
+		
+		pane.add(label, LeftCon);
+		pane.add(addressLine1Input, RightCon);
 		
 		return pane;
 	}
@@ -268,9 +319,13 @@ public class addCustomerView implements ActionListener {
 		
 		JPanel pane = new JPanel();
 		JLabel label = new JLabel("Address line 2");
+		label.setPreferredSize(PreferedDimension);
 		addressLine2Input = new JTextField(20);
-		pane.add(label);
-		pane.add(addressLine2Input);
+		
+		pane.setLayout(new GridBagLayout());
+		
+		pane.add(label, LeftCon);
+		pane.add(addressLine2Input, RightCon);
 		
 		return pane;
 	}
@@ -279,9 +334,13 @@ public class addCustomerView implements ActionListener {
 		
 		JPanel pane = new JPanel();
 		JLabel label = new JLabel("City");
+		label.setPreferredSize(PreferedDimension);
 		cityInput = new JTextField(20);
-		pane.add(label);
-		pane.add(cityInput);
+		
+		pane.setLayout(new GridBagLayout());
+		
+		pane.add(label, LeftCon);
+		pane.add(cityInput, RightCon);
 		
 		return pane;
 	}
@@ -290,9 +349,13 @@ public class addCustomerView implements ActionListener {
 		
 		JPanel pane = new JPanel();
 		JLabel label = new JLabel("State");
+		label.setPreferredSize(PreferedDimension);
 		stateInput = new JTextField(20);
-		pane.add(label); 
-		pane.add(stateInput); 
+		
+		pane.setLayout(new GridBagLayout());
+		
+		pane.add(label, LeftCon); 
+		pane.add(stateInput, RightCon); 
 		
 		return pane;
 	}
@@ -300,10 +363,14 @@ public class addCustomerView implements ActionListener {
 	private JPanel panelPostCode() {
 		
 		JPanel pane = new JPanel();
-		JLabel label = new JLabel("Postcode");		
+		JLabel label = new JLabel("Postcode");	
+		label.setPreferredSize(PreferedDimension);
 		postalCodeInput = new JTextField(20);
-		pane.add(label);
-		pane.add(postalCodeInput);
+		
+		pane.setLayout(new GridBagLayout());
+		
+		pane.add(label, LeftCon);
+		pane.add(postalCodeInput, RightCon);
 		
 		return pane;
 	}
@@ -311,10 +378,14 @@ public class addCustomerView implements ActionListener {
 	private JPanel panelCountry() {
 		
 		JPanel pane = new JPanel();
-		JLabel label = new JLabel("Country");		
+		JLabel label = new JLabel("Country");	
+		label.setPreferredSize(PreferedDimension);
 		countryInput = new JTextField(20);
-		pane.add(label);
-		pane.add(countryInput);
+		
+		pane.setLayout(new GridBagLayout());
+		
+		pane.add(label, LeftCon);
+		pane.add(countryInput, RightCon);
 		
 		return pane;
 	}
@@ -323,9 +394,13 @@ public class addCustomerView implements ActionListener {
 		
 		JPanel pane = new JPanel();
 		JLabel label = new JLabel("Employee Nr.");
+		label.setPreferredSize(PreferedDimension);
 	    salesRepNumberList = new JComboBox<Integer>(); updateCombobox();
-	    pane.add(label); 
-	    pane.add(salesRepNumberList); 
+	    
+	    pane.setLayout(new GridBagLayout());
+	    
+	    pane.add(label, LeftCon); 
+	    pane.add(salesRepNumberList, RightCon); 
 		
 		return pane;
 	}
@@ -334,10 +409,45 @@ public class addCustomerView implements ActionListener {
 		
 		JPanel pane = new JPanel();
 		JLabel label = new JLabel("Credit limit");
+		label.setPreferredSize(PreferedDimension);
 		creditLimitInput = new JTextField(20);
-		//creditLimitInput.addKeyListener(null);
-		pane.add(label); 
-		pane.add(creditLimitInput); 
+		creditLimitInput.addKeyListener(new KeyAdapter() {
+			
+			//Checks that the credits field only has 10 digits and 2 decimals
+			public void keyReleased(KeyEvent e) {
+				
+				JTextField textField = (JTextField) e.getSource();
+                String SearchString = textField.getText();
+                
+                //checks if the searchstring actually has anything written in it
+                if (SearchString.length() > 0) {
+                	
+                    textField.setText(SearchString.replaceAll("[^\\d.]", ""));
+                    double tempNumber = Double.parseDouble(SearchString);
+                    tempNumber = Math.round(tempNumber * 100d)/100d;
+                    
+                    //checks if the rounded number is longer than 10 digits
+                    if (String.valueOf(Math.round(tempNumber)).length() > 10) {
+                    	
+                    	responseText.setText("The credit number must be less than 10000000000");
+                    } else {
+                    	
+                    	responseText.setText(null);
+                    	CreditNumber = tempNumber;
+                    }
+                    
+                   
+                } else {
+                	
+                	CreditNumber = 0f;
+                }
+			}
+		});
+		
+		pane.setLayout(new GridBagLayout());
+		
+		pane.add(label, LeftCon); 
+		pane.add(creditLimitInput, RightCon); 
 		
 		return pane;
 	}
@@ -351,10 +461,11 @@ public class addCustomerView implements ActionListener {
 		addToDbButton.setBackground(Color.WHITE);
 	
 		resetButton = new JButton("Reset");
-		resetButton.setBounds(200, 420, 80, 25);
 		resetButton.addActionListener(this);
 		resetButton.setBackground(Color.WHITE);
 			
+		pane.setLayout(new GridLayout(2, 1));
+		
 		pane.add(addToDbButton);
 		pane.add(resetButton);
 		
